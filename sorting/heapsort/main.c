1771fdc5b72d3b30e9b26af5d1f5f274136d9d57
Dmitry Kozyrev <Ko3bIpb1625347@gmail.com>
2020-01-12 00:55:15 +0900

    heapsort change

--- sorting/heapsort/main.c
+++ sorting/heapsort/main.c
@@ -24,44 +24,35 @@ int main()
     return 0;
 }
 
+/* heapify: build heap */
 void heapify(int arr[], int n, int i)
 {   
     int largest = i;   
-// Инициализируем наибольший элемент как корень
-    int l = 2 * i + 1; // левый = 2*i + 1
-    int r = 2 * i + 2; // правый = 2*i + 2
 
- // Если левый дочерний элемент больше корня
+    int l = 2 * i + 1; // left
+    int r = 2 * i + 2; // right
+
     if (l < n && arr[l] > arr[largest])
         largest = l;
 
-   // Если правый дочерний элемент больше, чем самый большой элемент на данный момент
     if (r < n && arr[r] > arr[largest])
         largest = r;
 
-    // Если самый большой элемент не корень
     if (largest != i)
     {
         swap(&arr[i], &arr[largest]);
-
-// Рекурсивно преобразуем в двоичную кучу затронутое поддерево
         heapify(arr, n, largest);
     }
 }
-
-void heapSort(int arr[], int n)
+/* heapSort: sorting array */
+void heapSort(int arr[], int length)
 {
-  // Построение кучи (перегруппируем массив)
-    for (int i = n / 2 - 1; i >= 0; i--)
-        heapify(arr, n, i);
+    for (int i = length / 2 - 1; i >= 0 ; i--)
+        heapify(arr, length, i);
 
-   // Один за другим извлекаем элементы из кучи
-    for (int i=n-1; i>=0; i--)
+    for (int i = length - 1; i >= 0; i--)
     {
-        // Перемещаем текущий корень в конец
         swap(&arr[0], &arr[i]);
-
-        // вызываем процедуру heapify на уменьшенной куче
         heapify(arr, i, 0);
     }
 }
