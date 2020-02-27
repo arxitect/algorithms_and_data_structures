17c3d254c3e0d3cc98561a6764c8dfd19bb45a3b
Kozyrev Dmitry <Ko3bIpb1625347@gmail.com>
2020-02-27 14:00:37 +0900

    stack prepare

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Algorithms in the C programming language
+# Algorithms and data structures in the C programming language
 This repository implements algorithms and data structures in the C programming language.  
 
 Here are presented only the basic concepts for the implementation  understanding. You can use them as a basis in your projects, modifying and optimizing.
--- structures/stack/main.c
+++ structures/stack/main.c
@@ -1,7 +1,32 @@
+#include <stdio.h>
+
+#define MAX 10
+
+//push to stack
+void push(int *data,int n)
+{
+    int range = MAX;
+    while(range-- > 1){
+        
+    }
+}
+//pop from stack
+int pop()
+{
+    return 0;
+}
 
 int main()
 {   
-  
+    int data[MAX] = {0}; // stack
+
+    int i = 0;
+    while(i < 10){
+        printf("Enter some number: \n");
+        int n;
+        scanf("%d", &n);
+        push(data, n);
+    }
     return 0;
 }
 
