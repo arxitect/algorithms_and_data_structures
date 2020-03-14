8acd2f0f087d0eaca757d7a6028ae9548e3a302a
Dmitry Kozyrev <Ko3bIpb1625347@gmail.com>
2020-03-15 00:28:20 +0900

    counting sort review done, bucket sort recoding done, bucket sort review done

--- algorithms/bucket/main.c
+++ algorithms/bucket/main.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "data.h"
 
-void bucketSort(int arr[], int length);
+// struct of bucket
+struct bucket 
+{
+    int count;
+    int *values;
+};
+
+void bucketSort(int array[],int n);
+int compareIntegers(const void *first, const void *second);
 
 int main()
 {   
@@ -22,23 +31,42 @@ int main()
     return 0;
 }
 
-/* bucket sort */
-void bucketSort(int arr[], int n)
+int compareIntegers(const void* first, const void* second)
 {
-    int i, j;   
-    int count[n]; // new array with length of data elements
+    return *((int*)first) - *((int*)second);
+}
 
-    for(i = 0; i < n; i++){   
-        count[i] = 0;   
-    }     
+void bucketSort(int array[],int n)
+{
+    struct bucket buckets[3];
+    int i, j, k;
+    for (i = 0; i < 3; i++) {
+        buckets[i].count = 0;
+        buckets[i].values = (int*)malloc(sizeof(int) * n);
+    }
+    // Divide the unsorted elements among 3 buckets
+    // < 0    : first
+    // 0 - 10 : second
+    // > 10   : third
+    for (i = 0; i < n; i++) {
+        if (array[i] < 0){
+            buckets[0].values[buckets[0].count++] = array[i];
+        }
+        else if (array[i] > 10){
+            buckets[2].values[buckets[2].count++] = array[i];
+        }
+        else{
+            buckets[1].values[buckets[1].count++] = array[i];
+        }
+    }
 
-    for(i = 0; i < n; i++){
-        (count[arr[i]])++; // counting elems
-    }   
-
-    for(i = 0, j = 0; i < n; i++){   
-        for(; count[i] > 0; (count[i])--){       
-            arr[j++] = i; // insert elems to data
-        }  
+    for (k = 0, i = 0; i < 3; i++) {
+        // Use quicksort to sort each bucket individually
+        qsort(buckets[i].values, buckets[i].count, sizeof(int), compareIntegers);
+        for (j = 0; j < buckets[i].count; j++) {
+            array[k + j] = buckets[i].values[j];
+        }
+        k += buckets[i].count;
+        free(buckets[i].values);
     }
 }
--- algorithms/counting/main.c
+++ algorithms/counting/main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include "data.h"
 #include <string.h>
-#define RANGE 7
 
 void countSort(int arr[], int length);
+int getMax(int array[], int size);
 
 int main()
 {   
@@ -24,17 +24,32 @@ int main()
     return 0;
 }
 
+/* get max: pull max element of array */
+int getMax(int array[], int size)
+{
+    int max = array[0];
+    for (int i = 1; i < size; i++)
+        if (array[i] > max)
+            max = array[i];
+    return max;
+}
+
 /* count sort */
-void countSort(int* array, int n) {
-    int c[RANGE] = { 0 }; // array for counting
+void countSort(int* array, int n) 
+{
+    const int k = getMax(array, size);
+
+    int c[k]; // array for counting
+    for (int i = 0; i <= n; i++){
+        c[i] = 0;
+    }
 
     for (int i = 0; i < n; i++) {
         c[array[i]]++; // counting elements
     }
         
     int b = 0;
-
-    for (int i = 0; i < RANGE; i++) {
+    for (int i = 0; i < k; i++) {
         for (int j = 0; j < c[i]; j++) {
             array[b++] = i; // insert elem
         }
