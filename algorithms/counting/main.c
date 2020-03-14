03dcd2cf50bc89afbe09464167370bb6a9bdd764
Dmitry Kozyrev <Ko3bIpb1625347@gmail.com>
2020-03-14 23:41:48 +0900

    countino sort review

--- algorithms/counting/main.c
+++ algorithms/counting/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "data.h"
 #include <string.h>
-#define RANGE 255
+#define RANGE 7
 
 void countSort(int arr[], int length);
 
@@ -25,26 +25,18 @@ int main()
 }
 
 /* count sort */
-void countSort(int arr[],int length) 
-{ 
+void countSort(int* array, int n) {
+    int c[RANGE] = { 0 }; // array for counting
 
-    char output[length]; 
-  
-    int count[RANGE + 1], i; 
-    memset(count, 0, sizeof(count)); 
-  
-    for(i = 0; arr[i]; ++i) 
-        ++count[arr[i]]; 
+    for (int i = 0; i < n; i++) {
+        c[array[i]]++; // counting elements
+    }
+        
+    int b = 0;
 
-    for (i = 1; i <= RANGE; ++i) 
-        count[i] += count[i-1]; 
-  
-    for (i = 0; arr[i]; ++i) 
-    { 
-        output[count[arr[i]]-1] = arr[i]; 
-        --count[arr[i]]; 
-    } 
-  
-    for (i = 0; arr[i]; ++i) 
-        arr[i] = output[i]; 
-} 
+    for (int i = 0; i < RANGE; i++) {
+        for (int j = 0; j < c[i]; j++) {
+            array[b++] = i; // insert elem
+        }
+    }   
+}
