734f18b77f7a8240bf690c99173845da76e4789a
Kozyrev Dmitry <Ko3bIpb1625347@gmail.com>
2020-03-23 13:57:34 +0900

    qucksort review

--- algorithms/quickSort/main.c
+++ algorithms/quickSort/main.c
@@ -24,7 +24,8 @@ int main()
 
     return 0;
 }
-  
+
+/*partition: Redistribution of elements in the array in such a way that elements less than the reference are placed in front of it, and more or equal after. */
 int partition (int arr[], int low, int high) 
 { 
     int pivot = arr[high];
@@ -32,7 +33,7 @@ int partition (int arr[], int low, int high)
   
     for (int j = low; j < high; j++) 
     { 
-        if (arr[j] < pivot) 
+        if (arr[j] < pivot) //If smaller than the support element, place in front of it 
         {    
             swap(&arr[++i], &arr[j]); 
         } 
@@ -40,18 +41,20 @@ int partition (int arr[], int low, int high)
     swap(&arr[i + 1], &arr[high]); 
     return (i + 1); 
 } 
-  
+
+/*quick sort*/
 void quickSort(int arr[], int low, int high) 
 { 
     if (low < high) 
     { 
         int q = partition(arr, low, high); 
-  
+        //Recursive for left and right half
         quickSort(arr, low, q - 1); 
         quickSort(arr, q + 1, high); 
     } 
 }
 
+/* swap: swap two values*/
 void swap(int* a, int* b) 
 { 
     int t = *a; 
