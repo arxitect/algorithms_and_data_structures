a8292d95cd8a34a253c731608aaef13b25875273
Kozyrev Dmitry <Ko3bIpb1625347@gmail.com>
2020-03-25 13:52:45 +0900

    radix sort done

---
+++ algorithms/radixSort/main.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include "data.h"
+#include <string.h>
+
+int getMax(int arr[], int n);
+void countSort(int arr[], int n, int exp);
+void radixSort(int arr[], int n);
+
+int main()
+{   
+    int length = sizeof(data)/sizeof(int); // size of data
+
+    for( int i = 0; i < length; i++) // printing input data
+        printf("%d ", data[i]);
+
+    printf(" Input\n");
+    
+    radixSort(data, length);
+
+    for( int i = 0; i < length; i++) // printing input data
+        printf("%d ", data[i]);
+
+    printf(" Output\n");
+
+    return 0;
+}
+
+/* getMax: get max element of array */
+int getMax(int arr[], int n) 
+{ 
+    int mx = arr[0]; 
+    for (int i = 1; i < n; i++) 
+        if (arr[i] > mx) 
+            mx = arr[i]; 
+    return mx; 
+} 
+
+/*countSort: function to do counting sort of arr*/
+void countSort(int arr[], int n, int exp) 
+{ 
+    int output[n]; 
+    int i, count[10] = {0}; 
+   
+    for (i = 0; i < n; i++) 
+        count[ (arr[i]/exp)%10 ]++;  // Store count of occurrences in count[]
+  
+    for (i = 1; i < 10; i++) 
+        count[i] += count[i - 1]; //Change count[i] so that count[i] now contains actual position of this digit in output[] 
+  
+    // Build the output array 
+    for (i = n - 1; i >= 0; i--) { 
+        output[count[ (arr[i]/exp)%10 ] - 1] = arr[i]; 
+        count[ (arr[i]/exp)%10 ]--; 
+    } 
+  
+    for (i = 0; i < n; i++) 
+        arr[i] = output[i]; 
+} 
+/*radixSort*/
+void radixSort(int arr[], int n) 
+{ 
+    int m = getMax(arr, n); //get max element of array
+  
+    for (int exp = 1; m/exp > 0; exp *= 10) 
+        countSort(arr, n, exp); 
+}
