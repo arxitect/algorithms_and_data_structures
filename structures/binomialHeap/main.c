86e519b59f292f38b5936bf1c669603149968984
Kozyrev Dmitry <Ko3bIpb1625347@gmail.com>
2020-04-06 23:59:24 +0900

    new node create

--- structures/binomialHeap/main.c
+++ structures/binomialHeap/main.c
@@ -1,20 +1,32 @@
 #include <stdio.h>
 #include <malloc.h>
- 
-struct node {
-    int n; 
+
+typedef struct Node node;
+
+struct Node {
+    int data; 
     int degree;//count of child elements of node
-    struct node* parent; //pointer to parent node
-    struct node* child; //pointer to child node
-    struct node* sibling; //pointer to right brother of node
+    node* parent; //pointer to parent node
+    node* child; //pointer to child node
+    node* sibling; //pointer to right brother of node
 };
  
-makeBinomialHeap();
+node *newNode(int key);
+/*makeBinomialHeap();
 binomialHeapMinimum();
-binomialHeapUnion();
+binomialHeapUnion();*/
 
 int main()
 {
     
     return 0;
+}
+
+node *newNode(int key)
+{
+    node *temp = malloc(sizeof(node));
+    temp->data = key;
+    temp->degree = 0;
+    temp->child = temp->parent = temp->sibling = NULL;
+    return temp;
 }
