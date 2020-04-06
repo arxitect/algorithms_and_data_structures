fb4a04f16dbe850b6583975efedf6b544a80c617
Kozyrev Dmitry <Ko3bIpb1625347@gmail.com>
2020-04-07 00:33:26 +0900

    merge binomial trees, swap

--- structures/binomialHeap/main.c
+++ structures/binomialHeap/main.c
@@ -12,16 +12,19 @@ struct Node {
 };
  
 node *newNode(int key);
+node *mergeBinomialTrees(node *b1, node *b2);
+void swap(node *b1, node *b2);
 /*makeBinomialHeap();
 binomialHeapMinimum();
 binomialHeapUnion();*/
 
 int main()
 {
-    
+   
     return 0;
 }
 
+/*newNode: to create a new node of heap*/
 node *newNode(int key)
 {
     node *temp = malloc(sizeof(node));
@@ -29,4 +32,24 @@ node *newNode(int key)
     temp->degree = 0;
     temp->child = temp->parent = temp->sibling = NULL;
     return temp;
+}
+
+node *mergeBinomialTrees(node *b1, node *b2)
+{
+    if(b1->data > b2->data)
+        swap(b1, b2);
+
+    b2->parent = b1;
+    b2->sibling = b1->child;
+    b1->child = b2;
+    b1->degree++;
+}
+
+/*swap: swap two binomial trees*/
+void swap(node *b1, node *b2)
+{
+    node *temp = malloc(sizeof(node));
+    *temp = *b1;
+    *b1 = *b2;
+    *b2 = *temp;
 }
